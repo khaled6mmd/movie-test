455cf80bea44d90384479732611dbae1439ac43b
khaled6mmd <37240519+khaled6mmd@users.noreply.github.com>
2022-07-30 16:08:56 +0200

    before filter

--- src/app/components/movies-section/movies-section.component.ts
+++ src/app/components/movies-section/movies-section.component.ts
@@ -1,4 +1,8 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { NgbModal, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
+import { Movie } from 'src/app/models/content.model';
+import { ContentService } from 'src/app/services/content.service';
+import { AddEditMovieComponent } from '../add-edit-movie/add-edit-movie.component';
 
 @Component({
   selector: 'app-movies-section',
@@ -7,12 +11,37 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class MoviesSectionComponent implements OnInit {
 
-  @Input() movies: any;
-  constructor() { }
+  @Input() movies: Movie[] = [];
+  @Output() onMovieSaved: EventEmitter<any> = new EventEmitter();
+  categorys: any;
+
+  constructor(private modalService: NgbModal, private contentService: ContentService) { }
 
   ngOnInit(): void {
-    console.log(this.movies);
-    
   }
 
+  openAddEditModal(isEdit: boolean, movie?: Movie) {
+    const modal: NgbModalRef = this.modalService.open(AddEditMovieComponent, { ariaLabelledBy: 'modal-basic-title' })
+    modal.result.then((result) => {
+    }, (reason) => {
+      if (reason === 'saved') {
+        this.onMovieSaved.emit();
+      }
+    });
+    modal.componentInstance.isEdit = isEdit;
+    if (isEdit) {
+      modal.componentInstance.movie = movie;
+    }
+  }
+
+  getAllCategorys() {
+    this.contentService.getCategories().subscribe(res => {
+      this.categorys = res.message;
+    })
+
+  deleteMovie(movieId: number) {
+    this.contentService.deleteMovie(movieId).subscribe(res => {
+      this.onMovieSaved.emit();
+    })
+  }
 }
